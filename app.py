2d1b54c9ea4567dba2f170d1bc9c724f1ee36f2e
blakebrandon-hub <50201165+blakebrandon-hub@users.noreply.github.com>
2022-03-12 10:32:00 -0800

    Modified for compatibility with Heroku Postgres

--- app.py
+++ app.py
@@ -6,11 +6,12 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from forms import *
 import uuid
 from datetime import datetime
+from os import environ
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "thisisasecret"
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///C:\\Users\\18313\\Desktop\\progress_tracker\\database.db"
+app.config['SECRET_KEY'] = environ.get('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE')
 Bootstrap(app)
 db = SQLAlchemy(app)
 login_manager = LoginManager()
