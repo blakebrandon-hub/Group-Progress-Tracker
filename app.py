d731c5546f565698286e23dbb0a9b6f215f8e350
blakebrandon-hub <50201165+blakebrandon-hub@users.noreply.github.com>
2022-03-13 10:28:06 -0700

    Update app.py

--- app.py
+++ app.py
@@ -188,28 +188,30 @@ def view_board(board_id):
     board = Board.query.filter_by(public_id=board_id).first()
     collab = Collaborator.query.filter_by(board_id=board_id, user_id=current_user.username).first()
 
-    if board.private != "Public":
+    if board.private != None:
+        
+        if board.private != "Public":
 
-        if board.owner != current_user.username:
+            if board.owner != current_user.username:
 
-            if collab == None:
-                return "<h1>Error: Board is private. Must be creator or collaborator to view this board</h1>"
+                if collab == None:
+                    return "<h1>Error: Board is private. Must be creator or collaborator to view this board</h1>"
 
-            if collab.user_id != current_user.username:
+                if collab.user_id != current_user.username:
 
-                return "<h1>Error: Board is private. Must be creator or collaborator to view this board</h1>"
+                    return "<h1>Error: Board is private. Must be creator or collaborator to view this board</h1>"
 
-    collabs = Collaborator.query.filter_by(board_id=board_id).all()
+        collabs = Collaborator.query.filter_by(board_id=board_id).all()
 
-    groups = Group.query.filter_by(board_id=board_id).all()
+        groups = Group.query.filter_by(board_id=board_id).all()
 
-    tickets = Ticket.query.filter_by(board_id=board_id).all()
+        tickets = Ticket.query.filter_by(board_id=board_id).all()
 
-    assignees = Assignee.query.filter_by(board_id=board_id).all()
+        assignees = Assignee.query.filter_by(board_id=board_id).all()
 
-    return render_template('board/board.html', name=current_user.username, 
-        board=board, groups=groups, collabs=collabs, tickets=tickets, 
-        assignees=assignees)
+        return render_template('board/board.html', name=current_user.username, 
+            board=board, groups=groups, collabs=collabs, tickets=tickets, 
+            assignees=assignees)
 
 @app.route('/<board_id>/update', methods=['GET', 'POST'])
 @login_required
